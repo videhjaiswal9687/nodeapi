567bd2be2420b2550d2efbb7bc7a7504dfc2ddf1
videhjaiswal9687 <75520279+videhjaiswal9687@users.noreply.github.com>
2023-07-12 13:49:46 +0530

    Update connection.js

--- modal/connection.js
+++ modal/connection.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose')
 // var url = "mongodb://127.0.0.1:27017/mern10maydatabase"
-var url = "mongodb+srv://vjuniversal1:videh12345@cluster0.um5zbzw.mongodb.net/newdatabase"
+var url = "mongodb+srv://vjuniversal1:videh12345@cluster0.um5zbzw.mongodb.net/mern10maydatabase"
 /*
 Opens Mongoose's default connection to MongoDB
 */
